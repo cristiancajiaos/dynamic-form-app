2d6ccf20b840dd25a65e3d3a33ff717751f15971
Cristian Cajiao <cristian.cajiao@alumnos.usm.cl>
2020-02-19 11:19:13 -0300

    Creados los componentes DynamicForm y DynamicFormQuestion

--- src/app/dynamic-form/dynamic-form.component.ts
+++ src/app/dynamic-form/dynamic-form.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { QuestionBase } from './../question-base';
+import { Component, OnInit, Input } from '@angular/core';
+
+import { QuestionControlService } from './../question-control.service';
 
 @Component({
   selector: 'app-dynamic-form',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dynamic-form.component.css']
 })
 export class DynamicFormComponent implements OnInit {
+  @Input() questions: QuestionBase<string>[] = [];
+  form: FormGroup;
+  payLoad = '';
 
-  constructor() { }
+  constructor(private qcs: QuestionControlService) { }
 
   ngOnInit(): void {
+    this.form = this.qcs.toFormGroup(this.questions);
   }
 
+  onSubmit(): void {
+    this.payLoad = JSON.stringify(this.form.getRawValue());
+  }
 }
